6dd022207b307e10b8109a662772f17f89b37bf3
Timofy-g45 <vasilenkotima@gmail.com>
2024-12-08 10:57:57 +0200

    hi3

--- dog.py
+++ dog.py
@@ -1,32 +1,18 @@
 class Pet:
-    def __init__(self, name, species="Арчібальд"):
+    def __init__(self, name):
         self.name = name
-        self.species = species
-        self.hunger = 50
-        self.energy = 50
+        self.hunger = 5
 
-    def eat(self):
-        self.hunger = max(0, self.hunger - 10)
-        print(f"{self.name} поїв Голод {self.hunger}.")
-
-    def sleep(self):
-        self.energy = min(100, self.energy + 20)
-        print(f"{self.name} поспав Енергія {self.energy}.")
-
-    def play(self):
-        if self.energy >= 10:
-            self.energy -= 10
-            self.hunger += 10
-            print(f"{self.name} пограв Енергія {self.energy}, Голод: {self.hunger}.")
+    def feed(self):
+        print(f"{self.name} поїв")
+        self.hunger += 1
+        if self.hunger < 0:
+            self.hunger = 0
 
     def status(self):
-        print(f"{self.name} — {self.species}. Голод {self.hunger}, Енергія {self.energy}.")
+        print(f"{self.name} має голод: {self.hunger}")
 
-pet = Pet("Арчібальд", )
-pet.status()
-pet.eat()
-pet.play()
-pet.sleep()
-pet.status()
-
-print()
+my_pet = Pet("Барсик")
+my_pet.status()
+my_pet.feed()
+my_pet.status()
